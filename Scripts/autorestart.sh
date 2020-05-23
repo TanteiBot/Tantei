926bc6a3ea6f3188ec4292af8e186750a35f9ad0
N0D4N <disrespectful_mind65@yahoo.com>
2020-05-24 00:57:46 +0300

    Create autorestart scripts since ReconnectIndefinitely isn't good in DSharpPlus

---
+++ Scripts/autorestart.sh
@@ -0,0 +1,11 @@
+﻿﻿﻿#!/bin/sh
+# This script is taken from https://dsharpplus.github.io/articles/hosting/raspberry_pi.html
+# and slightly modified for this particular bot since i'm fine with hardcoded bot name.
+# I haven't testet it on linux yet.
+echo Using `which dotnet`
+while true
+do
+    dotnet "PaperMalKing.dll"
+    echo "Application crashed, restarting in 5 seconds..."
+    sleep 5
+done
