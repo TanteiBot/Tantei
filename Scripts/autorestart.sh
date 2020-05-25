1f3da5c599f55c01e6f3cd21e7a57e201a908b0a
N0D4N <50947930+N0D4N@users.noreply.github.com>
2020-05-25 12:31:01 +0300

    Change config to yaml (#16)
    * Closes #13 
    
    * Create YAML example config files
    
    * Create autorestart scripts since ReconnectIndefinitely isn't good in DSharpPlus
    
    * Added command to proper exiting, added handling for proper handling in bash script
    
    * Actually switch to YAML config

---
+++ Scripts/autorestart.sh
@@ -0,0 +1,17 @@
+﻿﻿﻿#!/bin/bash
+# This script is taken from https://dsharpplus.github.io/articles/hosting/raspberry_pi.html
+# and slightly modified for this particular bot since i'm fine with hardcoded bot name.
+# I haven't tested it on linux yet.
+echo Using `which dotnet`
+while true
+do
+    dotnet "PaperMalKing.dll"
+    exitcode=$?
+    if [ $"exitcode" == "0" ]
+    then
+    echo "Bot exited clearly, quitting"
+    break
+    fi
+    echo "Application crashed, restarting in 5 seconds..."
+    sleep 5
+done
