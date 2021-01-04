755aa054c3f3df49dc95f1b473fd765d1b84105c
N0D4N <disrespectful_mind65@yahoo.com>
2021-01-04 19:13:58 +0200

    Properly stop update providers on application shutdown

--- PaperMalKing/Services/Background/UpdateProvidersManagementService.cs
+++ PaperMalKing/Services/Background/UpdateProvidersManagementService.cs
@@ -12,7 +12,7 @@ namespace PaperMalKing.Services.Background
 		private readonly ILogger<UpdateProvidersManagementService> _logger;
 		private readonly IServiceProvider _serviceProvider;
 		private readonly UpdateProvidersConfigurationService _updateProvidersConfigurationService;
-		
+
 
 		public UpdateProvidersManagementService(ILogger<UpdateProvidersManagementService> logger, IServiceProvider serviceProvider,
 												UpdateProvidersConfigurationService updateProvidersConfigurationService)
@@ -29,16 +29,18 @@ namespace PaperMalKing.Services.Background
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
 			this._logger.LogInformation("Starting to wait for shutdown for cancelling update providers checking for updates");
-			await Task.Delay(Timeout.Infinite, stoppingToken);
-			var tasks = new Task[this._updateProvidersConfigurationService.Providers.Count];
-			var providers = this._updateProvidersConfigurationService.Providers.Values.ToArray();
-			for (var i = 0; i < providers.Length; i++)
+			await Task.Delay(Timeout.Infinite, stoppingToken).ContinueWith(async task =>
 			{
-				var provider = providers[i];
-				tasks[i] = provider.TriggerStoppingAsync();
-			}
-
-			await Task.WhenAll(tasks);
+				this._logger.LogInformation("Stopping update providers");
+				var tasks = new Task[this._updateProvidersConfigurationService.Providers.Count];
+				var providers = this._updateProvidersConfigurationService.Providers.Values.ToArray();
+				for (var i = 0; i < providers.Length; i++)
+				{
+					var provider = providers[i];
+					tasks[i] = provider.TriggerStoppingAsync();
+				}
+				await Task.WhenAll(tasks);
+			}, TaskContinuationOptions.OnlyOnCanceled);
 		}
 	}
 }
