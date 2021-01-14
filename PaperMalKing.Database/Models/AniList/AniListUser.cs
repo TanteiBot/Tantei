18508dd59c800643ebed968e5a61155560172304
N0D4N <disrespectful_mind65@yahoo.com>
2021-01-14 22:07:18 +0200

    Add AniList models to DB

---
+++ PaperMalKing.Database/Models/AniList/AniListUser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaperMalKing.Database.Models.AniList
+{
+    public sealed class AniListUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public ulong Id { get; init; }
+        
+        public long LastActivityTimestamp { get; init; }
+        
+        public long LastReviewTimestamp { get; init; }
+        
+        public ulong DiscordUserId { get; init; }
+
+        public DiscordUser DiscordUser { get; init; } = null!;
+
+        public List<AniListFavourite> Favourites { get; init; } = null!;
+    }
+}
---
+++ PaperMalKing.Database/Models/AniList/FavouriteType.cs
@@ -0,0 +1,11 @@
+﻿namespace PaperMalKing.Database.Models.AniList
+{
+    public enum FavouriteType : byte
+    {
+        Anime,
+        Manga,
+        Character,
+        Staff,
+        Studio
+    }
+}
