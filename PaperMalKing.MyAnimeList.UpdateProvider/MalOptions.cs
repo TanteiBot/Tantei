31791fec93b9e2e51e79968d34b36a6fa7a4a274
N0D4N <50947930+N0D4N@users.noreply.github.com>
2022-12-15 21:22:49 +0200

    Add ClientId to MalOptions and update template.appsettings.json

--- PaperMalKing.MyAnimeList.UpdateProvider/MalOptions.cs
+++ PaperMalKing.MyAnimeList.UpdateProvider/MalOptions.cs
@@ -1,5 +1,6 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021-2022 N0D4N
+
 using PaperMalKing.Common.Options;
 using PaperMalKing.MyAnimeList.Wrapper;
 
@@ -14,4 +15,6 @@ internal sealed class MalOptions : IRateLimitOptions<MyAnimeListClient>, ITimerO
 	public required int PeriodInMilliseconds { get; init; }
 
 	public required int DelayBetweenChecksInMilliseconds { get; init; }
+
+	public required string ClientId { get; init; }
 }
