da2320d2ff69c72facfcf5804626c6be3d6bb44a
N0D4N <50947930+N0D4N@users.noreply.github.com>
2022-12-28 11:47:30 +0200

    Enable random order for AniList

--- PaperMalKing.AniList.UpdateProvider/AniListUpdateProvider.cs
+++ PaperMalKing.AniList.UpdateProvider/AniListUpdateProvider.cs
@@ -53,7 +53,7 @@ internal sealed class AniListUpdateProvider : BaseUpdateProvider
 					 u.DiscordUser.Guilds.Any() && ((u.Features & AniListUserFeatures.AnimeList) != 0 ||
 												    (u.Features & AniListUserFeatures.MangaList) != 0 ||
 												    (u.Features & AniListUserFeatures.Favourites) != 0 ||
-												    (u.Features & AniListUserFeatures.Reviews) != 0)).ToArray())
+												    (u.Features & AniListUserFeatures.Reviews) != 0)).OrderBy(x => EF.Functions.Random()).ToArray())
 		{
 			if (cancellationToken.IsCancellationRequested)
 				break;
