5f90c192e2e737024b17d0c6c4d1f32ae6c386e8
N0D4N <50947930+N0D4N@users.noreply.github.com>
2023-01-06 14:56:35 +0200

    Add test infrastructure and code for testing MyAnimeList profile parser, but not push it since it contains sensitive data

--- src/PaperMalKing.MyAnimeList.Wrapper/Models/User.cs
+++ src/PaperMalKing.MyAnimeList.Wrapper/Models/User.cs
@@ -9,23 +9,23 @@ internal sealed class User
 {
 	private string? _avatarUrl;
 	private string? _profileUrl;
-	internal required string Username { get; init; }
+	public required string Username { get; init; }
 
-	internal string ProfileUrl => this._profileUrl ??= $"{Constants.PROFILE_URL}{this.Username}";
+	public string ProfileUrl => this._profileUrl ??= $"{Constants.PROFILE_URL}{this.Username}";
 
 	internal string AvatarUrl =>
 		this._avatarUrl ??=
 			$"{Constants.USER_AVATAR}{this.Id}.jpg?t={DateTimeOffset.UtcNow.ToUnixTimeSeconds()}";
 
-	internal uint Id { get; init; }
+	public uint Id { get; init; }
 
-	internal bool HasPublicAnimeUpdates => this.LatestAnimeUpdateHash is not null;
+	public bool HasPublicAnimeUpdates => this.LatestAnimeUpdateHash is not null;
 
-	internal bool HasPublicMangaUpdates => this.LatestMangaUpdateHash is not null;
+	public bool HasPublicMangaUpdates => this.LatestMangaUpdateHash is not null;
 
-	internal string? LatestMangaUpdateHash { get; init; }
+	public string? LatestMangaUpdateHash { get; init; }
 
-	internal string? LatestAnimeUpdateHash { get; init; }
+	public string? LatestAnimeUpdateHash { get; init; }
 
-	internal required UserFavorites Favorites { get; init; }
+	public required UserFavorites Favorites { get; init; }
 }
