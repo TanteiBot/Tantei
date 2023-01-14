abfaaf752ba95bcc20e26827de36c6a2e15c859c
N0D4N <50947930+N0D4N@users.noreply.github.com>
2023-01-14 20:59:36 +0200

    Cache anilist page info

--- src/PaperMalKing.AniList.Wrapper/Json/JsonUpperPolicyCase.cs
+++ src/PaperMalKing.AniList.Wrapper/Json/JsonUpperPolicyCase.cs
@@ -1,8 +1,9 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021-2022 N0D4N
+
 using System.Text.Json;
 
-namespace PaperMalKing.AniList.Wrapper;
+namespace PaperMalKing.AniList.Wrapper.Json;
 
 internal sealed class JsonUpperPolicyCase : JsonNamingPolicy
 {
--- src/PaperMalKing.AniList.Wrapper/Models/PageInfo.cs
+++ src/PaperMalKing.AniList.Wrapper/Models/PageInfo.cs
@@ -1,7 +1,12 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021-2022 N0D4N
+
+using System.Text.Json.Serialization;
+using PaperMalKing.AniList.Wrapper.Json;
+
 namespace PaperMalKing.AniList.Wrapper.Models;
 
+[JsonConverter(typeof(PageInfoConverter))]
 internal sealed class PageInfo
 {
 	public bool HasNextPage { get; init; }
