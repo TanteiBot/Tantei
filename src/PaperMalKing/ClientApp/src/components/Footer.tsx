ac7c7e2a5d68e35bfc3381bb4d6fa646594bfdd5
N0D4N <50947930+N0D4N@users.noreply.github.com>
2023-02-15 09:54:59 +0200

    Temp commit changes

---
+++ src/PaperMalKing/ClientApp/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () =>
+    <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <span>FooterText</span>
+        </div>
+    </footer>;
+
+export default Footer;
--- src/PaperMalKing/ClientApp/src/main.tsx
+++ src/PaperMalKing/ClientApp/src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+// Import our custom CSS
+import './styles/index.scss'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(
+        <App />
+);
