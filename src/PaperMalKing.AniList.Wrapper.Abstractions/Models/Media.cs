0f84a0e3cdfd32ab5be286eda56b328df84893df
N0D4N <50947930+N0D4N@users.noreply.github.com>
2023-07-04 10:56:39 +0300

    Address more fixes

--- src/PaperMalKing.AniList.Wrapper.Abstractions/Models/Media.cs
+++ src/PaperMalKing.AniList.Wrapper.Abstractions/Models/Media.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using PaperMalKing.AniList.Wrapper.Abstractions.Models.Enums;
 using PaperMalKing.AniList.Wrapper.Abstractions.Models.Interfaces;
