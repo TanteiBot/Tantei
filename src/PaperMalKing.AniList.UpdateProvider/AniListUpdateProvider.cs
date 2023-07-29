3cfddddfd55c08f9ff5abd732c8c20f493667850
N0D4N <50947930+N0D4N@users.noreply.github.com>
2023-07-29 11:24:21 +0300

    Disable filtering for inactive Anilist users

--- src/PaperMalKing.AniList.UpdateProvider/AniListUpdateProvider.cs
+++ src/PaperMalKing.AniList.UpdateProvider/AniListUpdateProvider.cs
@@ -48,16 +48,6 @@ internal sealed class AniListUpdateProvider : BaseUpdateProvider
 			return;
 		}
 
-		static bool FilterInactiveUsers(AniListUser user)
-		{
-			var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-			if (now - Math.Max(user.LastReviewTimestamp, user.LastActivityTimestamp) > TimeSpan.FromDays(5).TotalSeconds)
-			{
-				return now % 3 == 0;
-			}
-
-			return false;
-		}
 		using var db = this._dbContextFactory.CreateDbContext();
 		var users = db.AniListUsers.TagWith("Query users for update checking").TagWithCallSite().Where(u =>
 						  u.DiscordUser.Guilds.Any() && ((u.Features & AniListUserFeatures.AnimeList) != 0 ||
@@ -69,11 +59,6 @@ internal sealed class AniListUpdateProvider : BaseUpdateProvider
 		{
 			if (cancellationToken.IsCancellationRequested)
 				break;
-			if (FilterInactiveUsers(dbUser))
-			{
-				this.Logger.LogDebug("Skipping update for {UserId}, because his last update was at {Timestamp}", dbUser.Id, DateTimeOffset.FromUnixTimeSeconds(Math.Max(dbUser.LastActivityTimestamp, dbUser.LastReviewTimestamp)));
-				continue;
-			}
 			using var perUserCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 			perUserCancellationTokenSource.CancelAfter(TimeSpan.FromMinutes(3));
 			var perUserCancellationToken = perUserCancellationTokenSource.Token;
