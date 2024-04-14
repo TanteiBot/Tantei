6de66dd197e0daea3e78aaadcf4f896588f1b5a3
N0D4N <50947930+N0D4N@users.noreply.github.com>
2024-04-14 20:41:57 +0300

    Remove usings

--- src/PaperMalKing.Startup/Services/Background/UpdateProvidersManagementService.cs
+++ src/PaperMalKing.Startup/Services/Background/UpdateProvidersManagementService.cs
@@ -1,7 +1,6 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021-2024 N0D4N
 
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
