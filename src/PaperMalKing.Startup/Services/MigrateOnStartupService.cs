49e999dc358c18c4aea83536a2b7b014a5f85455
N0D4N <50947930+N0D4N@users.noreply.github.com>
2024-04-19 22:58:48 +0300

    Apply database migrations on startup

---
+++ src/PaperMalKing.Startup/Services/MigrateOnStartupService.cs
@@ -0,0 +1,29 @@
+﻿// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021-2024 N0D4N
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PaperMalKing.Database;
+using PaperMalKing.UpdatesProviders.Base;
+
+namespace PaperMalKing.Startup.Services;
+
+internal sealed class MigrateOnStartupService : IExecuteOnStartupService
+{
+	private readonly IDbContextFactory<DatabaseContext> _factory;
+
+	public MigrateOnStartupService(IDbContextFactory<DatabaseContext> factory)
+	{
+		this._factory = factory;
+	}
+
+	public async Task ExecuteAsync(CancellationToken cancellationToken = default)
+	{
+		await using var db = this._factory.CreateDbContext();
+
+		db.Database.Migrate();
+
+		await Task.CompletedTask;
+	}
+}
