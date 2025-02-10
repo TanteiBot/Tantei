bf0ebf9729853a0a52ff3e0513e8708f39a214b9
N0D4N <50947930+N0D4N@users.noreply.github.com>
2025-02-10 22:40:23 +0200

    Add test to verify we didn't miss EF changes (#827)

--- tests/Tantei.Tests/UngroupedTests.cs
+++ tests/Tantei.Tests/UngroupedTests.cs
@@ -2,6 +2,8 @@
 // Copyright (C) 2021-2024 N0D4N
 
 using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+using PaperMalKing.Database;
 
 namespace Tantei.Tests;
 
@@ -13,5 +15,13 @@ public sealed class UngroupedTests
 		_ = DateTimeOffset.ParseExact(GitVersionInformation.CommitDate, "o", CultureInfo.InvariantCulture);
 
 		Assert.True(true);
+	}
+
+	[Fact]
+	public async Task DbSchemaHaveNotChanged()
+	{
+		await using var db = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseSqlite().Options);
+		var schema = db.Database.GenerateCreateScript();
+		await Verify(schema);
 	}
 }
