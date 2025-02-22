bb0dd6cad4a2d67ff4accb581e7b931ca4ef6ea5
N0D4N <50947930+N0D4N@users.noreply.github.com>
2025-02-22 22:56:52 +0200

    Add test to verify no pending migrations changes

---
+++ tests/Tantei.Tests/EfCoreTests.cs
@@ -0,0 +1,27 @@
+﻿// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021-2024 N0D4N
+
+using Microsoft.EntityFrameworkCore;
+using PaperMalKing.Database;
+
+namespace Tantei.Tests;
+
+public class EfCoreTests
+{
+	[Fact]
+	public async Task DbSchemaHaveNotChanged()
+	{
+		await using var db = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseSqlite().Options);
+		var schema = db.Database.GenerateCreateScript();
+		await Verify(schema);
+	}
+
+	[Fact]
+	public async Task DbNoPendingMigrationsAreLeft()
+	{
+		await using var dbContext = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>()
+														.UseSqlite(x => x.MigrationsAssembly("PaperMalKing.Database.Migrations")).Options);
+
+		Assert.False(dbContext.Database.HasPendingModelChanges());
+	}
+}
