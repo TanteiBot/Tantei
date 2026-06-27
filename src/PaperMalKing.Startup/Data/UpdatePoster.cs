eeb750a27c3b7a494d27bb30a8529c4e9ac066f8
N0D4N <50947930+N0D4N@users.noreply.github.com>
2026-06-27 14:25:58 +0300

    FIX3

--- src/PaperMalKing.Startup/Data/UpdatePoster.cs
+++ src/PaperMalKing.Startup/Data/UpdatePoster.cs
@@ -1,7 +1,6 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021-2026 N0D4N
 
-using System.Diagnostics.CodeAnalysis;
 using DSharpPlus.Entities;
 using Microsoft.Extensions.Logging;
 using Microsoft.IO;
