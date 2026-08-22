da29223c1012da4463252f5b98a4dc592bb0dea5
N0D4N <50947930+N0D4N@users.noreply.github.com>
2026-08-22 13:11:03 +0300

    FIX CI

--- src/PaperMalKing.UpdatesProviders.Base/Colors/CustomColorService.cs
+++ src/PaperMalKing.UpdatesProviders.Base/Colors/CustomColorService.cs
@@ -2,7 +2,6 @@
 // Copyright (C) 2021-2026 N0D4N
 
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.CompilerServices;
 using DSharpPlus.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -60,7 +59,6 @@ public sealed class CustomColorService<[DynamicallyAccessedMembers(DynamicallyAc
 			return null;
 		}
 
-		return $"Your colors: {colors.Select(c =>
-			string.Create(CultureInfo.InvariantCulture, $"{(TUpdateType)(object)c.UpdateType}: #{c.ColorValue:X6}")).JoinToString('\n')}";
+		return $"Your colors: {colors.Select(c => $"{(TUpdateType)(object)c.UpdateType}: #{c.ColorValue:X6}").JoinToString('\n')}";
 	}
 }
